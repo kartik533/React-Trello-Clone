9c8f98ded91c73555faa61f87a455af6ea11a5a6
kartik533 <kartik533@gmail.com>
2021-06-13 19:19:27 +0530

    Add eslint to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
         <h1>
             Hello World!
         </h1>
     </div>
-  );
-}
+)
 
-export default App;
+export default App
